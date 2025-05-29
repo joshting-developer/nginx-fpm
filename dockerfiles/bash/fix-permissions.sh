0bc628948f7f8268724d340e670d72697646b021
Josh_Chen <muki.josh1999@gmail.com>
2025-05-29 15:00:14 +0800

    調整資料夾權限修改方式 - 排除volumes

--- dockerfiles/bash/fix-permissions.sh
+++ dockerfiles/bash/fix-permissions.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-chown -R www-data:www-data /var/www/html
+
+WEB_ROOT="/var/www/html"
+EXCLUDE_DIR="volumes"
+
+find "$WEB_ROOT" -path "$WEB_ROOT/$EXCLUDE_DIR" -prune -o -exec chown www-data:www-data {} \;
+
 chown -R www-data:www-data /var/log
 exec "$@"
